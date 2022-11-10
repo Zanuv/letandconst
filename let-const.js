3eb212ef2987529c85919d90ddac34cbfe63b26c
Zach <zanuv@outlook.com>
2022-11-10 04:14:17 -0600

    first commit

---
+++ let-const.js
@@ -0,0 +1,20 @@
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+// ES2015 Global Constants
+const PI = 3.14;
+
+// Quiz
+// What is the difference between var and let?
+// var is function scoped and let is block scoped
+// var can be redeclared while let cannot be redeclared
+
+// What is the difference between var and const?
+// const is block scope, const cannot be reassinged or redeclared
+
+// What is the difference between let and const?
+// let can be reassigned while const cannot
+
+// What is hoisting?
+// Hoisting is when variables are used before they are declared
